c457c09af712a1e85ad34b491ea6e3ac34b39d4e
John <nagato.leet@gmail.com>
2022-09-22 16:18:30 +0800

    Added Game mechanics.

---
+++ Thesis/Assets/Scripts/Find The Letter/ClickableLetter.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class ClickableLetter : MonoBehaviour, IPointerClickHandler
+{
+    char _randomLetter;
+    bool _upperCase;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Debug.Log($"Clicked on letter {_randomLetter}");
+        if (_randomLetter == GameControll.Instance.Letter)
+        {
+            GetComponent<TMP_Text>().color = Color.green;
+            enabled = false;
+            GameControll.Instance.HandleCorrectLetterClick(_upperCase);
+        }else
+        {
+            GetComponent<TMP_Text>().color = Color.red;
+            enabled = false;
+            Debug.Log($"Clicked the wrong letter");
+        }
+    }
+
+    public void SetLetter(char letter)
+    {
+        enabled = true;
+        GetComponent<TMP_Text>().color = Color.black;
+        _randomLetter = letter;
+
+        if (Random.Range(0, 100) > 50)
+        {
+            _upperCase = false;
+            GetComponent<TMP_Text>().text = _randomLetter.ToString();
+        }
+        else
+        {
+            _upperCase = true;
+            GetComponent<TMP_Text>().text = _randomLetter.ToString().ToUpper();
+        }
+    }
+}
---
+++ Thesis/Assets/Scripts/Find The Letter/GameControll.cs
@@ -0,0 +1,104 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GameControll : MonoBehaviour
+{
+    [SerializeField] List<AudioClip> _audioClips;
+
+    public char Letter = 'a';
+    
+    int _correctAnswers = 5;
+    int _correctClicks;
+    
+    public static GameControll Instance { get; private set; }
+
+    AudioSource _audioSource;
+
+    void Awake()
+    {
+        Instance = this;
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnEnable()
+    {
+        GenerateBoard();
+        UpdateDiplayLetters();
+    }
+
+    void GenerateBoard()
+    {
+        var clickables = FindObjectsOfType<ClickableLetter>();
+
+        List<char> charsList = new List<char>();
+
+        for (int i = 0; i < _correctAnswers; i++)
+            charsList.Add(Letter);
+
+        for (int i = _correctAnswers; i < clickables.Length; i++)
+        {
+            var chosenLetter = ChooseInvalidRandomLetter();
+            charsList.Add(chosenLetter);
+        }
+
+        charsList = charsList
+            .OrderBy(t => UnityEngine.Random.Range(0, 10000))
+            .ToList();
+
+        for (int i = 0; i < clickables.Length; i++)
+        {
+            clickables[i].SetLetter(charsList[i]);
+        }
+
+        FindObjectOfType<RemainingCounterText>().SetRemaining(_correctAnswers - _correctClicks);
+    }
+
+    internal void HandleCorrectLetterClick(bool upperCase)
+    {
+        var clip = _audioClips.FirstOrDefault(t => t.name == Letter.ToString());
+        if (upperCase)
+            clip = _audioClips.FirstOrDefault(t => t.name == Letter.ToString() + Letter.ToString());
+
+        _audioSource.PlayOneShot(clip);
+
+        _correctClicks++;
+        FindObjectOfType<RemainingCounterText>().SetRemaining(_correctAnswers - _correctClicks);
+        if (_correctClicks >= _correctAnswers)
+        {
+            MoveToNextLetter();
+            UpdateDiplayLetters();
+            _correctClicks = 0;
+            GenerateBoard();
+        }
+    }
+
+    private void MoveToNextLetter()
+    {
+        Letter++;
+        var clip = _audioClips.FirstOrDefault(t => t.name == Letter.ToString());
+        if (clip == null)
+            Letter = 'a';
+    }
+
+    private void UpdateDiplayLetters()
+    {
+        foreach (var displayletter in FindObjectsOfType<DisplayLetter>())
+        {
+            displayletter.SetLetter(Letter);
+        }
+    }
+
+    private char ChooseInvalidRandomLetter()
+    {
+        int a = UnityEngine.Random.Range(0, 26);
+        var randomLetter = (char)('a' + a);
+        while (randomLetter == Letter)
+        {
+            a = UnityEngine.Random.Range(0, 26);
+            randomLetter = (char)('a' + a);
+        }
+
+        return randomLetter;
+    }
+}
