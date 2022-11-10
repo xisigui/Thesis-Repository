e496360bb10358f53208689d8ded250c0cbff3bf
John Philipp <nagato.leet@gmail.com>
2022-11-10 10:48:02 +0800

    Fix Errors

--- Top Down/Assets/Script/Save&Load/PlayerData.cs
+++ Top Down/Assets/Script/Save&Load/PlayerData.cs
@@ -6,9 +6,9 @@ using UnityEngine;
 public class PlayerData
 {
     public float[] position;
-    public PlayerData (Player player)
+    public PlayerData (PlayerMovement player)
     {
-        position = new float[3]
+        position = new float[3];
         position[0] = player.transform.position.x;
         position[1] = player.transform.position.y;
         position[2] = player.transform.position.z;
--- Top Down/Assets/Script/Save&Load/SerializationManager.cs
+++ Top Down/Assets/Script/Save&Load/SerializationManager.cs
@@ -4,7 +4,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 
 public class SerializationManager
 {
-    public static void Save(Player player)
+    public static void Save(PlayerMovement player)
     {
         if(!Directory.Exists(Application.persistentDataPath + "/saves"))
         {
@@ -21,18 +21,29 @@ public class SerializationManager
 
     public static object Load(string path)
     {
-        string path = Application.persistentDataPath + "/saves";
+        path = Application.persistentDataPath + "/saves";
         if(!File.Exists(path))
         {
             Debug.LogError("Save file not found in " + path);
             return null;
         }
-        
+
         BinaryFormatter formatter = GetBinaryFormatter();
-        FileStream stream = new FileStream(path, FileMode.Open);
-        PlayerData data = formatter.Deserialize(stream) as PlayerData;
-        stream.Close;
-        return data;
+        FileStream stream = File.Open(path, FileMode.Open);
+
+        try
+        {           
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+
+            stream.Close();
+            return data;
+        }
+        catch
+        {
+            Debug.LogError("Failed to load data at " + path);
+            stream.Close();
+            return null;
+        }        
     }
 
     public static BinaryFormatter GetBinaryFormatter()
