8fbea3a50b7537a813b8a04ee5ae5c3e7b64f090
MarcRubenEpacta <marcrubenepacta@gmail.com>
2022-11-20 03:21:52 +0800

    New map added

--- Top Down/Assets/Script/MainMenu/ButtonEvents.cs
+++ Top Down/Assets/Script/MainMenu/ButtonEvents.cs
@@ -11,7 +11,7 @@ public class ButtonEvents : MonoBehaviour
 
     public void Quit()
     {
-        UnityEditor.EditorApplication.isPlaying = false;
+        //UnityEditor.EditorApplication.isPlaying = false;
         Application.Quit();
     }
 }
