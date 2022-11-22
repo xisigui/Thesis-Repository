5fdfc78cd0be4dfb15aa771e809f7940e38a95a0
John Philipp <nagato.leet@gmail.com>
2022-11-22 21:09:52 +0800

    Saving layer fixe

--- Top Down/Assets/Script/Save&Load/PlayerData.cs
+++ Top Down/Assets/Script/Save&Load/PlayerData.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class PlayerData
 {
     public float[] position;
+    public string sortingLayer;
     public PlayerData (Player player)
     {
         position = new float[3];
         position[0] = player.transform.position.x;
         position[1] = player.transform.position.y;
         position[2] = player.transform.position.z;
+
+        sortingLayer = player.GetComponent<SpriteRenderer>().sortingLayerName;
     }
 }
