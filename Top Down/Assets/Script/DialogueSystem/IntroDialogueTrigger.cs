349a57f8de4c27bae7d035cbc42cb002b5a7b362
MarcRubenEpacta <marcrubenepacta@gmail.com>
2022-11-28 08:24:10 +0800

    Intro cutscene and Intro dialogue added

---
+++ Top Down/Assets/Script/DialogueSystem/IntroDialogueTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogueTrigger : MonoBehaviour
+{
+    private bool istriggered = false;
+    public GameObject disableObject; 
+    public DialogueTrigger trigger;
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(istriggered == false){
+            if(collision.gameObject.CompareTag("Player") == true){
+                trigger.StartDialogue();
+            }
+            istriggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        istriggered = false;
+        disableObject.SetActive(false);
+    }
+}
