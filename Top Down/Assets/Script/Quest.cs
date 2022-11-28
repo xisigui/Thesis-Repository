5a9c92da4cd2cea1d652b0877a955fcad6eb0013
MarcRubenEpacta <marcrubenepacta@gmail.com>
2022-11-28 12:43:17 +0800

    area annotations

---
+++ Top Down/Assets/Script/Quest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour
+{
+    public Quest[] allQuest;
+     
+    void FinishQuest(){
+
+    }
+}
