5693ac47e87ecb83a0272fca631de2c13f97d6fa
John Philipp <nagato.leet@gmail.com>
2022-12-16 17:14:50 +0800

    Added clicked word checker & finish message

--- Top Down/Assets/Script/PuzzlePrototype/GameController.cs
+++ Top Down/Assets/Script/PuzzlePrototype/GameController.cs
@@ -1,12 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    string Word = "Google";
     int _correctAnswers = 5;
+    int _correctClicks = 0;
     string[] nounDictionary = { "Family", "People", "Friend", "Year", "Teacher", "Student" };
+    string[] fillDictionary = { "Blank", "Blank", "Blank", "Blank", "Blank", "Blank" };
 
     void OnEnable()
     {
@@ -15,8 +16,40 @@ public class GameController : MonoBehaviour
     void GenerateBoard()
     {
         var clickables = FindObjectsOfType<ClickableWord>();
+        List<string> wordList = new List<string>();
+
+        for (int i = 0; i < _correctAnswers; i++)
+            wordList.Add(nounDictionary[i]);
+
+        //Not final, Only used to populate the wordsList to fill all clickable words.
+        for(int i = 0; i < clickables.Length - _correctAnswers; i++)
+            wordList.Add(fillDictionary[Random.Range(0, fillDictionary.Length)]); 
+
+        wordList = wordList.OrderBy(t => UnityEngine.Random.Range(0, 10000)).ToList();
 
         for (int i = 0; i < clickables.Length; i++)
-            clickables[i].SetLetter(nounDictionary[Random.Range(0,nounDictionary.Length)]);     
+        {
+            clickables[i].SetLetter(wordList[i]);
+        }   
+        FindObjectOfType<RemainingCounterText>().SetRemaining(_correctAnswers);                     
+    }
+
+    public void checkWord(string word)
+    {
+        for (int i = 0;i<nounDictionary.Length ;i++)
+        {
+            if(word.Equals(nounDictionary[i]))
+            {
+                _correctClicks++;
+                FindObjectOfType<CorrectCounterText>().SetCorrectCount(_correctClicks);
+            }                
+        }
+
+        if (_correctClicks >= _correctAnswers)
+        {
+            Debug.Log("All Noun has been found");
+            _correctClicks = 0;
+        }
+        
     }
 }
