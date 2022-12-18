d7b93d309dea717b0b682e16fb27d749aaed8e1b
MarcRubenEpacta <marcrubenepacta@gmail.com>
2022-12-18 22:12:49 +0800

    match by type added prefabs

--- Top Down/Assets/Script/MatchingType/MatchLogic.cs
+++ Top Down/Assets/Script/MatchingType/MatchLogic.cs
@@ -11,6 +11,8 @@ public class MatchLogic : MonoBehaviour
     public int maxPoints = 3;
     public TMP_Text pointText;
     public GameObject levelCompleteUI;
+    public GameObject continueButton;
+    public GameObject gameCanvas;
     private int points = 0;
 
 
@@ -22,6 +24,7 @@ public class MatchLogic : MonoBehaviour
         pointText.text = points + "/" + maxPoints;
         if(points == maxPoints){
             levelCompleteUI.SetActive(true);
+            continueButton.SetActive(true);
         }
     }
 
@@ -32,5 +35,9 @@ public class MatchLogic : MonoBehaviour
     public static void AddPoints(int points){
         Instance.points += points;
         Instance.UpdatePointstext();
+    }
+
+    public void ContinueButton(){
+        gameCanvas.SetActive(false);
     }
 }
